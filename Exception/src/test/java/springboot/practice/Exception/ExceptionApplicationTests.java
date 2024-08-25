665a2abf2cd226c30da9b3a4ab7e6196ac002dd5
Vicky <Vicky@DESKTOP-V7F5G4J>
2024-08-25 15:59:55 +0530

    Excep commit

---
+++ Exception/src/test/java/springboot/practice/Exception/ExceptionApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.practice.Exception;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
